8cffb885f06d3e82bdc6d3bddedecbbcd730a730
st195473 <stefano.golzio@edu.unito.it>
2025-12-24 11:24:31 +0100

    removed unused endpoint get_poster, API ready for heroku deploy

--- solution/banana_bean/src/main/java/com/ripe_bananas/banana_bean/repository/PostersRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ripe_bananas.banana_bean.repository;
-
-import com.ripe_bananas.banana_bean.entity.Poster;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostersRepo extends JpaRepository<Poster, Integer> {
-}
