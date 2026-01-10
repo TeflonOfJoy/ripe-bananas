dae7c6c9f9c8c9a1f4df629f886a14b3116a8f8e
st195473 <stefano.golzio@edu.unito.it>
2026-01-10 15:51:22 +0100

    added @Entity annotation in OscarAward.java and added OscarAwardsRepo and OscarAwardsService

--- solution/banana_bean/src/main/java/com/ripe_bananas/banana_bean/entity/OscarAward.java
+++ solution/banana_bean/src/main/java/com/ripe_bananas/banana_bean/entity/OscarAward.java
@@ -1,6 +1,7 @@
 package com.ripe_bananas.banana_bean.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
 @Table(name = "oscar_awards")
 public class OscarAward {
   @Id
